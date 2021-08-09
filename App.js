5b6894a795481d8b785a98c9e88340b784572016
user <user@users-MacBook-Pro.local>
2021-08-09 11:22:43 +0700

    add bottom tabbar

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 import {Home, OrderDelivery, Restaurant} from './screens';
+import Tabs from './navigation/Tabs';
 import {Text, View} from 'react-native';
 
 const Stack = createStackNavigator();
@@ -12,11 +13,12 @@ const App = () => {
       <Stack.Navigator
         screenOptions={{headerShown: false}}
         initialRouteName={'Home'}>
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Home" component={Tabs} />
         <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
         <Stack.Screen name="Restaurant" component={Restaurant} />
       </Stack.Navigator>
     </NavigationContainer>
   );
+
 };
 export default App;

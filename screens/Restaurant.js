7cb46431ab76d6d0e4e367a0a73cce06ac55aa05
user <user@users-MacBook-Pro.local>
2021-08-09 10:43:28 +0700

    setup navigation

---
+++ screens/Restaurant.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+const Restaurant = () => {
+  return (
+    <View>
+      <Text>Restaurant</Text>
+    </View>
+  );
+};
+export default Restaurant;

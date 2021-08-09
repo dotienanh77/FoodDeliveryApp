008edcd765f2f6b5e2347eedb3a57a44a75f7f2b
user <user@users-MacBook-Pro.local>
2021-08-09 15:35:37 +0700

    custom botton tabbar

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -5,16 +5,91 @@ import {
   createBottomTabNavigator,
   BottomTabBar,
 } from '@react-navigation/bottom-tabs';
+import Svg, {Path} from 'react-native-svg';
+import {isIphoneX} from 'react-native-iphone-x-helper';
 import {Home} from '../screens';
 import {COLORS, icons} from '../constants';
 
 const Tab = createBottomTabNavigator();
+const TabBarCustomButton = ({accessibilityState, children, onPress}) => {
+  let isSelected = accessibilityState.selected;
+
+  if (isSelected) {
+    return (
+      <View style={{flex: 1, alignItems: 'center'}}>
+        <View style={{flexDirection: 'row', position: 'absolute', top: 0}}>
+          <View style={{flex: 1, backgroundColor: COLORS.white}} />
+          <Svg width={75} height={61} viewBox="0 0 75 61">
+            <Path
+              d="M75.2 0v61H0V0c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1h-.1z"
+              fill={COLORS.white}
+            />
+          </Svg>
+          <View style={{flex: 1, backgroundColor: COLORS.white}} />
+        </View>
+        <TouchableOpacity
+          style={{
+            top: -22.5,
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: 50,
+            height: 50,
+            borderRadius: 25,
+            backgroundColor: COLORS.white,
+          }}
+          onPress={onPress}>
+          {children}
+        </TouchableOpacity>
+      </View>
+    );
+  } else {
+    return (
+      <TouchableOpacity
+        style={{
+          flex: 1,
+          height: 60,
+          backgroundColor: COLORS.white,
+        }}
+        activeOpacity={1}
+        onPress={onPress}>
+        {children}
+      </TouchableOpacity>
+    );
+  }
+};
+const CustomTabBar = props => {
+  if (isIphoneX()) {
+    return (
+      <View>
+        <View
+          style={{
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            right: 0,
+            height: 30,
+            backgroundColor: COLORS.white,
+          }}
+        />
+        <BottomTabBar {...props.props} />
+      </View>
+    );
+  } else {
+    return <BottomTabBar {...props.props} />;
+  }
+};
 const Tabs = () => {
   return (
     <Tab.Navigator
       tabBarOptions={{
         showLabel: false,
-      }}>
+        style: {
+          borderTopWidth: 0,
+          backgroundColor: 'transparent',
+          elevation: 0, // for android
+        },
+      }}
+      tabBar={props => <CustomTabBar props={props} />}>
       <Tab.Screen
         name="Home"
         component={Home}
@@ -30,6 +105,7 @@ const Tabs = () => {
               }}
             />
           ),
+          tabBarButton: props => <TabBarCustomButton {...props} />,
         }}
       />
       <Tab.Screen
@@ -47,6 +123,7 @@ const Tabs = () => {
               }}
             />
           ),
+          tabBarButton: props => <TabBarCustomButton {...props} />,
         }}
       />
       <Tab.Screen
@@ -64,6 +141,7 @@ const Tabs = () => {
               }}
             />
           ),
+          tabBarButton: props => <TabBarCustomButton {...props} />,
         }}
       />
       <Tab.Screen
@@ -81,6 +159,7 @@ const Tabs = () => {
               }}
             />
           ),
+          tabBarButton: props => <TabBarCustomButton {...props} />,
         }}
       />
     </Tab.Navigator>

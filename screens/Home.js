4305e24d418a655d47eb1a68af70969fdef003a2
user <user@users-MacBook-Pro.local>
2021-08-10 20:17:47 +0700

    done Home screen, set navigation to restaurant screen

--- screens/Home.js
+++ screens/Home.js
@@ -10,7 +10,7 @@ import {
   FlatList,
 } from 'react-native';
 import {COLORS, FONTS, SIZES, icons, images} from '../constants';
-const Home = () => {
+const Home = ({navigation}) => {
   // Dummy Datas
 
   const initialCurrentLocation = {
@@ -85,7 +85,7 @@ const Home = () => {
       name: 'ByProgrammers Burger',
       rating: 4.8,
       categories: [5, 7],
-      priceRating: affordable,
+      priceRating: fairPrice,
       photo: images.burger_restaurant_1,
       duration: '30 - 45 min',
       location: {
@@ -339,6 +339,13 @@ const Home = () => {
     setRestaurants(restaurantList);
     setSelectedCategory(category);
   }
+  function getCategoryNameById(id) {
+    let category = categories.filter(a => a.id === id);
+    if (category.length > 0) {
+      return category[0].name;
+    }
+    return '';
+  }
   function renderHeader() {
     return (
       <View style={{flexDirection: 'row', height: 50}}>
@@ -443,10 +450,99 @@ const Home = () => {
       </View>
     );
   }
+  function renderRestaurantList() {
+    const renderItem = ({item}) => (
+      <TouchableOpacity
+        style={{marginBottom: SIZES.padding * 2}}
+        // onPress
+        onPress={() =>
+          navigation.navigate('Restaurant', {item, currentLocation})
+        }>
+        <View style={{marginBottom: SIZES.padding * 2}}>
+          <Image
+            source={item.photo}
+            resizeMode="cover"
+            style={{width: '100%', height: 200, borderRadius: SIZES.radius}}
+          />
+          <View
+            style={{
+              position: 'absolute',
+              bottom: 0,
+              height: 50,
+              width: SIZES.width * 0.3,
+              backgroundColor: COLORS.white,
+              borderTopRightRadius: SIZES.radius,
+              borderBottomLeftRadius: SIZES.radius,
+              alignItems: 'center',
+              justifyContent: 'center',
+              ...styles.shadow,
+            }}>
+            <Text style={{...FONTS.h4}}>{item.duration}</Text>
+          </View>
+        </View>
+        {/* Restaurant info */}
+        <Text style={{...FONTS.body2}}>{item.name}</Text>
+        <View style={{marginTop: SIZES.padding, flexDirection: 'row'}}>
+          {/* Rating */}
+          <Image
+            source={icons.star}
+            style={{
+              height: 20,
+              width: 20,
+              tintColor: COLORS.primary,
+              marginRight: 10,
+            }}
+          />
+          <Text style={{...FONTS.body3}}>{item.rating}</Text>
+          <View style={{flexDirection: 'row', marginLeft: 10}}>
+            {item.categories.map(categoryId => {
+              return (
+                <View style={{flexDirection: 'row'}} key={categoryId}>
+                  <Text style={{...FONTS.body3}}>
+                    {getCategoryNameById(categoryId)}
+                  </Text>
+                  <Text style={{...FONTS.body3, color: COLORS.darkgray}}>
+                    {' '}
+                    .{' '}
+                  </Text>
+                </View>
+              );
+            })}
+            {/* Price */}
+            {[1, 2, 3].map(priceRating => (
+              <Text
+                key={priceRating}
+                style={{
+                  ...FONTS.body3,
+                  color:
+                    priceRating <= item.priceRating
+                      ? COLORS.black
+                      : COLORS.darkgray,
+                }}>
+                $
+              </Text>
+            ))}
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+    return (
+      <FlatList
+        data={restaurants}
+        keyExtractor={item => `${item.id}`}
+        renderItem={renderItem}
+        contentContainerStyle={{
+          paddingHorizontal: SIZES.padding * 2,
+          paddingBottom: 30,
+        }}
+      />
+    );
+  }
   return (
     <SafeAreaView style={styles.container}>
       {renderHeader()}
       {renderMainCategories()}
+      {renderRestaurantList()}
     </SafeAreaView>
   );
 };

1f4a05050240bbde6640894d0f1a1202913e35f8
user <user@users-MacBook-Pro.local>
2021-08-10 15:38:05 +0700

    add flatlist main menu

--- screens/Home.js
+++ screens/Home.js
@@ -375,7 +375,67 @@ const Home = () => {
       </View>
     );
   }
-  return <SafeAreaView style={styles.container}>{renderHeader()}</SafeAreaView>;
+  function renderMainCategories() {
+    const renderItem = ({item}) => {
+      return (
+        <TouchableOpacity
+          style={{
+            padding: SIZES.padding,
+            paddingBottom: SIZES.padding * 2,
+            backgroundColor: COLORS.primary,
+            borderRadius: SIZES.radius,
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginRight: SIZES.padding,
+            ...styles.shadow,
+          }}>
+          <View
+            style={{
+              width: 50,
+              height: 50,
+              borderRadius: 25,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: COLORS.white,
+            }}>
+            <Image
+              source={item.icon}
+              resizeMode="contain"
+              style={{width: 30, height: 30}}
+            />
+          </View>
+          <Text
+            style={{
+              marginTop: SIZES.padding,
+              color: COLORS.white,
+              ...FONTS.body5,
+            }}>
+            {item.name}
+          </Text>
+        </TouchableOpacity>
+      );
+    };
+    return (
+      <View style={{padding: SIZES.padding * 2}}>
+        <Text style={{...FONTS.h1}}>Main</Text>
+        <Text style={{...FONTS.h1}}>Categories</Text>
+        <FlatList
+          data={categories}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={item => `${item.id}`}
+          renderItem={renderItem}
+          contentContainerStyle={{paddingVertical: SIZES.padding * 2}}
+        />
+      </View>
+    );
+  }
+  return (
+    <SafeAreaView style={styles.container}>
+      {renderHeader()}
+      {renderMainCategories()}
+    </SafeAreaView>
+  );
 };
 const styles = StyleSheet.create({
   container: {

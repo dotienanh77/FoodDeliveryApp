1836f0d437cdbb99c979cb4bd0c387924d6627b2
user <user@users-MacBook-Pro.local>
2021-08-10 10:49:09 +0700

    add data

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
-import {Home, OrderDelivery, Restaurant} from './screens';
+
+import {Restaurant, OrderDelivery} from './screens';
 import Tabs from './navigation/Tabs';
-import {Text, View} from 'react-native';
 
 const Stack = createStackNavigator();
 
@@ -11,14 +12,16 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        screenOptions={{headerShown: false}}
+        screenOptions={{
+          headerShown: false,
+        }}
         initialRouteName={'Home'}>
         <Stack.Screen name="Home" component={Tabs} />
-        <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
         <Stack.Screen name="Restaurant" component={Restaurant} />
+        <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-
 };
+
 export default App;
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,25 +1,28 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {Text, View, Image, TouchableOpacity} from 'react-native';
+import {View, Image, TouchableOpacity} from 'react-native';
 import {
   createBottomTabNavigator,
   BottomTabBar,
 } from '@react-navigation/bottom-tabs';
 import Svg, {Path} from 'react-native-svg';
 import {isIphoneX} from 'react-native-iphone-x-helper';
+
 import {Home} from '../screens';
+
 import {COLORS, icons} from '../constants';
 
 const Tab = createBottomTabNavigator();
+
 const TabBarCustomButton = ({accessibilityState, children, onPress}) => {
-  let isSelected = accessibilityState.selected;
+  var isSelected = accessibilityState.selected;
 
   if (isSelected) {
     return (
       <View style={{flex: 1, alignItems: 'center'}}>
         <View style={{flexDirection: 'row', position: 'absolute', top: 0}}>
           <View style={{flex: 1, backgroundColor: COLORS.white}} />
-          <Svg width={75} height={61} viewBox="0 0 75 61">
+          <Svg width={70} height={61} viewBox="0 0 75 61">
             <Path
               d="M75.2 0v61H0V0c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1h-.1z"
               fill={COLORS.white}
@@ -27,6 +30,7 @@ const TabBarCustomButton = ({accessibilityState, children, onPress}) => {
           </Svg>
           <View style={{flex: 1, backgroundColor: COLORS.white}} />
         </View>
+
         <TouchableOpacity
           style={{
             top: -22.5,
@@ -57,6 +61,7 @@ const TabBarCustomButton = ({accessibilityState, children, onPress}) => {
     );
   }
 };
+
 const CustomTabBar = props => {
   if (isIphoneX()) {
     return (
@@ -78,17 +83,23 @@ const CustomTabBar = props => {
     return <BottomTabBar {...props.props} />;
   }
 };
+
 const Tabs = () => {
   return (
     <Tab.Navigator
       tabBarOptions={{
         showLabel: false,
         style: {
+          position: 'absolute',
+          left: 0,
+          bottom: 0,
+          right: 0,
           borderTopWidth: 0,
           backgroundColor: 'transparent',
-          elevation: 0, // for android
+          elevation: 0,
         },
       }}
+      screenOptions={{headerShown: false}}
       tabBar={props => <CustomTabBar props={props} />}>
       <Tab.Screen
         name="Home"
@@ -108,6 +119,7 @@ const Tabs = () => {
           tabBarButton: props => <TabBarCustomButton {...props} />,
         }}
       />
+
       <Tab.Screen
         name="Search"
         component={Home}
@@ -126,6 +138,7 @@ const Tabs = () => {
           tabBarButton: props => <TabBarCustomButton {...props} />,
         }}
       />
+
       <Tab.Screen
         name="Like"
         component={Home}
@@ -144,6 +157,7 @@ const Tabs = () => {
           tabBarButton: props => <TabBarCustomButton {...props} />,
         }}
       />
+
       <Tab.Screen
         name="User"
         component={Home}
@@ -165,4 +179,5 @@ const Tabs = () => {
     </Tab.Navigator>
   );
 };
+
 export default Tabs;

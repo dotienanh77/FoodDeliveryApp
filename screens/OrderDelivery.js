32163a9d94d2ebb92e3f9e735580cb7f35456192
user <user@users-MacBook-Pro.local>
2021-08-11 20:10:23 +0700

    add pin icon on map

--- screens/OrderDelivery.js
+++ screens/OrderDelivery.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {Text, View, SafeAreaView, Image, TouchableOpacity} from 'react-native';
@@ -12,22 +13,66 @@ const OrderDelivery = ({route, navigation}) => {
   const [region, setRegion] = React.useState(null);
 
   React.useEffect(() => {
-    //pass over from restaurant screen
     let {restaurant, currentLocation} = route.params;
-  },[])
+
+    let fromLoc = currentLocation.gps;
+    let toLoc = restaurant.location;
+    let street = currentLocation.streetName;
+
+    let mapRegion = {
+      latitude: (fromLoc.latitude + toLoc.latitude) / 2,
+      longitude: (fromLoc.longitude + toLoc.longitude) / 2,
+      latitudeDelta: Math.abs(fromLoc.latitude - toLoc.latitude) * 2,
+      longitudeDelta: Math.abs(fromLoc.longitude - toLoc.longitude) * 2,
+    };
+
+    setRestaurant(restaurant);
+    setStreetName(street);
+    setFromLocation(fromLoc);
+    setToLocation(toLoc);
+    setRegion(mapRegion);
+  }, []);
 
   function renderMap() {
+    const destinationMarker = () => (
+      <Marker coordinate={toLocation}>
+        <View
+          style={{
+            height: 40,
+            width: 40,
+            borderRadius: 20,
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: COLORS.white,
+          }}>
+          <View
+            style={{
+              height: 30,
+              width: 30,
+              borderRadius: 15,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: COLORS.primary,
+            }}>
+            <Image
+              source={icons.pin}
+              style={{width: 25, height: 25, tintColor: COLORS.white}}
+            />
+          </View>
+        </View>
+      </Marker>
+    );
     return (
       <View style={{flex: 1}}>
-        <MapView style={{flex: 1}}>
-          
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          initialRegion={region}
+          style={{flex: 1}}>
+          {destinationMarker()}
         </MapView>
       </View>
     );
   }
-  return (
-    
-      <View style={{flex: 1}}>{renderMap()}</View>
-  );
+  return <View style={{flex: 1}}>{renderMap()}</View>;
 };
 export default OrderDelivery;

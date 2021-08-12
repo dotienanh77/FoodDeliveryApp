09b76a08975f2eb6e32918087e15dbea651868da
user <user@users-MacBook-Pro.local>
2021-08-12 15:48:17 +0700

    maps direction, destination box

--- screens/OrderDelivery.js
+++ screens/OrderDelivery.js
@@ -2,16 +2,22 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {Text, View, SafeAreaView, Image, TouchableOpacity} from 'react-native';
-import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 import {COLORS, SIZES, FONTS, icons, GOOGLE_API_KEY} from '../constants';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 
 const OrderDelivery = ({route, navigation}) => {
+  const mapView = React.useRef();
+
   const [restaurant, setRestaurant] = React.useState(null);
   const [streetName, setStreetName] = React.useState('');
   const [fromLocation, setFromLocation] = React.useState(null);
   const [toLocation, setToLocation] = React.useState(null);
   const [region, setRegion] = React.useState(null);
 
+  const [duration, setDuration] = React.useState(0);
+  const [isReady, setIsReady] = React.useState(false);
+  const [angle, setAngle] = React.useState(0);
   React.useEffect(() => {
     let {restaurant, currentLocation} = route.params;
 
@@ -32,6 +38,17 @@ const OrderDelivery = ({route, navigation}) => {
     setToLocation(toLoc);
     setRegion(mapRegion);
   }, []);
+
+  function calculateAngle(coordinates) {
+    let startLat = coordinates[0].latitude;
+    let startLng = coordinates[0].longitude;
+    let endLat = coordinates[1].latitude;
+    let endLng = coordinates[1].longitude;
+    let dx = endLat - startLat;
+    let dy = endLng - startLng;
+
+    return (Math.atan2(dy, dx) * 180) / Math.PI;
+  }
 
   function renderMap() {
     const destinationMarker = () => (
@@ -62,17 +79,103 @@ const OrderDelivery = ({route, navigation}) => {
         </View>
       </Marker>
     );
+    const carIcon = () => (
+      <Marker
+        coordinate={fromLocation}
+        anchor={{x: 0.5, y: 0.5}}
+        flat={true}
+        rotation={angle}>
+        <Image source={icons.car} style={{width: 40, height: 40}} />
+      </Marker>
+    );
     return (
       <View style={{flex: 1}}>
         <MapView
+          ref={mapView}
           provider={PROVIDER_GOOGLE}
           initialRegion={region}
           style={{flex: 1}}>
+          <MapViewDirections
+            origin={fromLocation}
+            destination={toLocation}
+            apikey={GOOGLE_API_KEY}
+            strokeWidth={5}
+            strokeColor={COLORS.primary}
+            optimizeWaypoints={true}
+            onReady={result => {
+              setDuration(result.duration);
+              if (!isReady) {
+                // fit route into maps
+                mapView.current.fitToCoordinates(result.coordinates, {
+                  edgePadding: {
+                    right: SIZES.width / 20,
+                    bottom: SIZES.height / 4,
+                    left: SIZES.width / 20,
+                    top: SIZES.height / 8,
+                  },
+                });
+                // reposition the car
+                let nextLoc = {
+                  latitude: result.coordinates[0].latitude,
+                  longitude: result.coordinates[0].longtitude,
+                };
+                if (result.coordinates.length >= 2) {
+                  let angle = calculateAngle(result.coordinates);
+                  setAngle(angle);
+                }
+                setFromLocation(nextLoc);
+                setIsReady(true);
+              }
+            }}
+          />
           {destinationMarker()}
+          {carIcon()}
         </MapView>
       </View>
     );
   }
-  return <View style={{flex: 1}}>{renderMap()}</View>;
+  function renderDextinationHeader() {
+    return (
+      <View
+        style={{
+          position: 'absolute',
+          top: 50,
+          right: 0,
+          left: 0,
+          height: 50,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            width: SIZES.width * 0.9,
+            paddingVertical: SIZES.padding,
+            paddingHorizontal: SIZES.padding * 2,
+            borderRadius: SIZES.radius,
+            backgroundColor: COLORS.white,
+          }}>
+          <Image
+            source={icons.red_pin}
+            style={{
+              width: 30,
+              height: 30,
+              marginRight: SIZES.padding,
+            }}
+          />
+          <View style={{flex: 1}}>
+            <Text style={{...FONTS.body3}}>{streetName}</Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
+  return (
+    <View style={{flex: 1}}>
+      {renderMap()}
+      {renderDextinationHeader()}
+    </View>
+  );
 };
 export default OrderDelivery;
